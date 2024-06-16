80841464009bdc3539bf9508fce34ee02e432833
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2024-06-16 09:33:35 +0200

    Add react router and separater notebooks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React, { createRef } from 'react';
-import ProblemDetail from './pages/ProblemDetail/ProblemDetail'
-import GridSquares from './games/GridSquares/GridSquares';
-
-import * as markdowns from './markdown_texts';
-import * as templates from './code_templates';
-
 function App() {
-  let ref = createRef<GridSquares>();
   return (
     <div className="App">
-      <ProblemDetail markdown_text={markdowns.WELCOME_MARKDOWN} template_code={templates.CUBES_CODE} game_ref={ref}>
-        <GridSquares ref={ref} gridHeight={3} gridWidth={10} />
-      </ProblemDetail>
+      <ul>
+        <li><a href={`/noteboooks/1`}>Cubes Notebook</a></li>
+        <li><a href={`/noteboooks/2`}>Turtle Notebook</a></li>
+        <li><a href={`/noteboooks/3`}>MIPS Notebook</a></li>
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -6,13 +6,48 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import GridSquares from './games/GridSquares/GridSquares';
+import Turtles from './games/Turtle/Turtle';
+import ProblemDetail from './pages/ProblemDetail/ProblemDetail';
+import * as markdowns from './markdown_texts';
+import * as templates from './code_templates';
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
+let ref1 = createRef<GridSquares>();
+let ref2 = createRef<Turtles>();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/noteboooks/1",
+    element: (
+    <ProblemDetail markdown_text={markdowns.WELCOME_MARKDOWN} template_code={templates.CUBES_CODE} game_ref={ref1}>
+      <GridSquares ref={ref1} gridHeight={3} gridWidth={10} />
+    </ProblemDetail>
+    )
+  },
+  {
+    path: "/noteboooks/2",
+    element: (
+    <ProblemDetail markdown_text={markdowns.TURTLE_MARKDOWN} template_code={templates.TURTLE_CODE} game_ref={ref2}>
+      <Turtles ref={ref2} areaHeight={100} areaWidth={100} />
+    </ProblemDetail>
+    )
+  }
+]);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
