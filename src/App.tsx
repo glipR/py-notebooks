121a16caffe1cc5017cf0721655fa88b9b4dd963
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2024-07-06 22:10:38 +0100

    Fix local dev + links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
+import { Link } from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
       <ul>
-        <li><a href={`/notebooks/1`}>Cubes Notebook</a></li>
-        <li><a href={`/notebooks/2`}>Turtle Notebook</a></li>
-        <li><a href={`/notebooks/3`}>Terminal Notebook</a></li>
+        <li><Link to='/notebooks/1'>Cubes Notebook</Link></li>
+        <li><Link to='/notebooks/2'>Turtle Notebook</Link></li>
+        <li><Link to='/notebooks/3'>Terminal Notebook</Link></li>
       </ul>
 
       <h2>Intro</h2>
       <ul>
-        <li><a href={`/notebooks/intro/1`}>Intro 1 - Data</a></li>
-        <li><a href={`/notebooks/intro/2`}>Intro 2 - Numbers Challenge #1</a></li>
-        <li><a href={`/notebooks/intro/3`}>Intro 3 - Numbers Challenges #2 and #3</a></li>
+        <li><Link to='/notebooks/intro/1'>Intro 1 - Data</Link></li>
+        <li><Link to='/notebooks/intro/2'>Intro 2 - Numbers Challenge #1</Link></li>
+        <li><Link to='/notebooks/intro/3'>Intro 3 - Numbers Challenges #2 and #3</Link></li>
       </ul>
       <h2>Strings</h2>
       <ul>
-        <li><a href={`/notebooks/strings/1`}>Strings 1 - Print vs. "Print"</a></li>
-        <li><a href={`/notebooks/strings/2`}>Strings 2 - Strings Challenge</a></li>
+        <li><Link to='/notebooks/strings/1'>Strings 1 - Print vs. "Print"</Link></li>
+        <li><Link to='/notebooks/strings/2'>Strings 2 - Strings Challenge</Link></li>
       </ul>
       <h2>Variables</h2>
       <ul>
-        <li><a href={`/notebooks/variables/1`}>Variables 1 - Intro</a></li>
-        <li><a href={`/notebooks/variables/2`}>Variables 2 - Challenge #1</a></li>
-        <li><a href={`/notebooks/variables/3`}>Variables 3 - Challenge #2</a></li>
-        <li><a href={`/notebooks/variables/4`}>Variables 4 - Variables defining themselves?</a></li>
-        <li><a href={`/notebooks/variables/5`}>Variables 5 - Challenge #3</a></li>
+        <li><Link to='/notebooks/variables/1'>Variables 1 - Intro</Link></li>
+        <li><Link to='/notebooks/variables/2'>Variables 2 - Challenge #1</Link></li>
+        <li><Link to='/notebooks/variables/3'>Variables 3 - Challenge #2</Link></li>
+        <li><Link to='/notebooks/variables/4'>Variables 4 - Variables defining themselves?</Link></li>
+        <li><Link to='/notebooks/variables/5'>Variables 5 - Challenge #3</Link></li>
       </ul>
       <h2>Calling Functions</h2>
       <ul>
-        <li><a href={`/notebooks/functions/1`}>Functions 1 - Intro</a></li>
-        <li><a href={`/notebooks/functions/2`}>Functions 2 - Return Values</a></li>
+        <li><Link to='/notebooks/functions/1'>Functions 1 - Intro</Link></li>
+        <li><Link to='/notebooks/functions/2'>Functions 2 - Return Values</Link></li>
       </ul>
       <h2>Conditions</h2>
       <ul>
-        <li><a href={`/notebooks/conditions/1`}>Conditions 1 - Intro</a></li>
-        <li><a href={`/notebooks/conditions/2`}>Conditions 2 - Combining Conditions</a></li>
-        <li><a href={`/notebooks/conditions/3`}>Conditions 3 - Extra If Tools</a></li>
-        <li><a href={`/notebooks/conditions/4`}>Conditions 4 - Challenge - Keys</a></li>
+        <li><Link to='/notebooks/conditions/1'>Conditions 1 - Intro</Link></li>
+        <li><Link to='/notebooks/conditions/2'>Conditions 2 - Combining Conditions</Link></li>
+        <li><Link to='/notebooks/conditions/3'>Conditions 3 - Extra If Tools</Link></li>
+        <li><Link to='/notebooks/conditions/4'>Conditions 4 - Challenge - Keys</Link></li>
       </ul>
     </div>
   );
